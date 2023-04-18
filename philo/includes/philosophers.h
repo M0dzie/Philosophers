65a89156e2b88accc430baabfe1ab84cc1902feb
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-04-18 15:07:22 +0200

    wanna die

--- philo/includes/philosophers.h
+++ philo/includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: thmeyer < thmeyer@student.42lyon.fr >      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 13:23:23 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/04/17 14:25:13 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/04/18 14:52:47 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,21 @@
 # include <pthread.h>
 # include <sys/time.h>
 
-typedef struct s_pdata
+typedef struct s_data
 {
 	long			nbr_philo;
 	long			time_to_die;
 	long			time_to_eat;
 	long			time_to_sleep;
 	long			nbr_must_eat;
+	long			time;
 	pthread_mutex_t	*fork;
-}					t_pdata;
+}					t_data;
 
 typedef struct s_philo
 {
+	int		id;
+	t_data	*data;
 }			t_philo;
 
 int		wrong_arg(char **argv);
--- philo/sources/main.c
+++ philo/sources/main.c
@@ -6,13 +6,22 @@
 /*   By: thmeyer < thmeyer@student.42lyon.fr >      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 13:23:23 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/04/17 14:45:37 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/04/18 15:07:00 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
-static void	init_data(t_pdata *data, char **argv)
+static void	init_time(t_data *data)
+{
+	struct timeval	time;
+
+	gettimeofday(&time, NULL);
+	data->time = time.tv_sec;
+	printf("time = %ld\n", time.tv_sec);
+}
+
+static void	init_data(t_data *data, char **argv)
 {
 	data->nbr_philo = ft_atoi(argv[1]);
 	data->time_to_die = ft_atoi(argv[2]);
@@ -20,35 +29,46 @@ static void	init_data(t_pdata *data, char **argv)
 	data->time_to_sleep = ft_atoi(argv[4]);
 	if (argv[5])
 		data->nbr_must_eat = ft_atoi(argv[5]);
-	data->fork = malloc(sizeof(pthread_mutex_t) * (data->nbr_philo + 1));
-	if (!data->fork)
-		return ;
 }
 
-static void	*start_routine(void *index)
+static void	*start_routine(void *arg)
 {
-	long	n;
+	t_philo	*philo;
 
-	n = (long)index;
-	printf("philo n%ld\n", n);
+	philo = (t_philo *)arg;
+	if (philo->id % 2 == 0)
+	{
+		// printf("je suis le philo %d\n", philo->id + 1);
+		pthread_mutex_lock(&philo->data->fork[philo->id - 1]);
+		printf("Philo %d has taken a fork\n", philo->id + 1);
+		pthread_mutex_lock(&philo->data->fork[philo->id]);
+		printf("Philo %d has taken a fork\n", philo->id + 1);
+		printf("Philo %d is eating\n", philo->id + 1);
+		pthread_mutex_unlock(&philo->data->fork[philo->id - 1]);
+		pthread_mutex_unlock(&philo->data->fork[philo->id]);
+	}
+	else
+		printf("ca marche R\n");
 	return (NULL);
 }
 
-static void	*create_thread(t_pdata *data)
+static void	*create_thread(t_data *data)
 {
-	long		i;
-	pthread_t	*philo;
+	int				i;
+	t_philo			philo[data->nbr_philo];
+	pthread_t		config[data->nbr_philo];
+	pthread_mutex_t	fork[data->nbr_philo];
 
+	data->fork = fork;
+	philo->data = data;
 	i = -1;
-	philo = malloc(sizeof(pthread_t) * (data->nbr_philo + 1));
-	if (!philo)
-		return (NULL);
 	while (++i < data->nbr_philo)
-	{	
-		pthread_create(&philo[i], NULL, start_routine, (void *)i);
-		pthread_join(philo[i], NULL);
-		pthread_mutex_init(&data->fork[i], NULL);
-		pthread_mutex_lock(&data->fork[i]);
+	{
+		philo[i].id = i;
+		printf("i = %d\n", i);
+		pthread_mutex_init(&philo->data->fork[i], NULL);
+		pthread_create(&config[i], NULL, start_routine, &philo[i]);
+		pthread_join(config[i], NULL);
 		usleep(30);
 	}
 	return (NULL);
@@ -56,7 +76,7 @@ static void	*create_thread(t_pdata *data)
 
 int	main(int argc, char **argv)
 {
-	t_pdata		data;
+	t_data	data;
 
 	if (argc != 5 && argc != 6)
 		return (display_error("Wrong number of arguments, you should have :\n\
@@ -64,7 +84,9 @@ int	main(int argc, char **argv)
 and eventually <number of times each philosophers must eat>"), 1);
 	if (wrong_arg(argv))
 		return (1);
+	init_time(&data);
 	init_data(&data, argv);
 	create_thread(&data);
 	return (0);
 }
+	
