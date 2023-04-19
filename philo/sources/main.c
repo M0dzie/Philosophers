d62a5d16e830c20916745c871d4eb1f084fbd033
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-04-19 14:31:35 +0200

    still wip, bug with last philo

--- philo/sources/main.c
+++ philo/sources/main.c
@@ -6,7 +6,7 @@
 /*   By: thmeyer < thmeyer@student.42lyon.fr >      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 13:23:23 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/04/19 14:04:52 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/04/19 14:22:12 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,12 +51,15 @@ static void	*forks_and_eat(t_philo *philo)
 	display_msg(philo, 0);
 	pthread_mutex_lock(&philo->data->fork[philo->index]);
 	display_msg(philo, 1);
-	pthread_mutex_lock(&philo->data->fork[philo->index - 1]);
+	if (philo->index == philo->data->nbr_must_eat - 1)
+		pthread_mutex_lock(&philo->data->fork[0]);
+	else
+		pthread_mutex_lock(&philo->data->fork[philo->index + 1]);
 	display_msg(philo, 1);
 	display_msg(philo, 2);
 	usleep(philo->data->time_to_eat * 1000);
 	pthread_mutex_unlock(&philo->data->fork[philo->index]);
-	pthread_mutex_unlock(&philo->data->fork[philo->index - 1]);
+	pthread_mutex_unlock(&philo->data->fork[philo->index + 1]);
 	display_msg(philo, 3);
 	usleep(philo->data->time_to_sleep * 1000);
 	return (NULL);
@@ -95,7 +98,7 @@ static void	*create_thread(t_data *data)
 		philo[i].index = i;
 		philo[i].data = data;
 		pthread_create(&ph_thread[i], NULL, start_routine, (void *)&philo[i]);
-		// usleep(50);
+		usleep(50);
 	}
 	i = -1;
 	while (++i < data->nbr_philo)
